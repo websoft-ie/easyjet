ef4e587edfc92220c7a7ff393758907b142f7b7d
websoft-ie <info@websoft.ie>
2019-10-12 13:47:55 +0100

    init repo
    init repo

---
+++ Interview/Interview/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<Test_T<Test>, Test> rep = new Repository<Test_T<Test>, Test>();
+
+            //SAVE 1 item in repository
+            Test_T<Test> test_1 = new Test_T<Test>();
+            test_1.Id = new Test();
+            rep.Save(test_1);
+
+            //GETALL items in repository. There is 1 item
+            foreach (var item in rep.GetAll())
+            {
+                Console.WriteLine(item.Id.GetHashCode().ToString());
+            }
+
+            //GET
+            var res = rep.Get(test_1.Id);
+
+            //DELETE one item
+            rep.Delete(test_1.Id);
+        }
+    }
+
+    public class Test
+    {
+
+    }
+
+    class Test_T<Test> : IStoreable<Test> // Implements interface IStoreable
+    {
+        Test id;
+        public Test Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+    }
+
+}
---
+++ Interview/Interview/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    public class Repository<T, I> : IRepository<T, I> where T : IStoreable<I>
+    {
+        private readonly List<T> _repository;
+        public Repository()
+        {
+            _repository = new List<T>();
+        }
+
+        public void Delete(I id)
+        {
+            _repository.RemoveAll(new Predicate<T>(z => z.Id.Equals(id)));
+        }
+        public T Get(I id)
+        {
+            return _repository.Find(new Predicate<T>(z => z.Id.Equals(id)));
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repository;
+        }
+
+        public void Save(T item)
+        {
+            _repository.Add(item);
+        }
+    }
+
+}
